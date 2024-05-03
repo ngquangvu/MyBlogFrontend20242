1ebb1bddd00dd71f72dd83b07250e51fbd6e342d
ngquangvu <nguyenquangvu.work@gmail.com>
2024-05-03 23:22:44 +0700

    update links

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -51,3 +51,22 @@ export function appendUrlParam(key: string, value: string) {
   }
   return '';
 }
+
+// UnCrypt mailto
+function unCryptMailto(s: string) {
+  var n = 0;
+  var r = "";
+  for (var i = 0; i < s.length; i++) {
+    n = s.charCodeAt(i);
+    if (n >= 8364) {
+      n = 128;
+    }
+    r += String.fromCharCode(n - 1);
+  }
+  return r;
+}
+
+// Link to unCryptMailto
+export function linkToUnCryptMailto(s: string) {
+  location.href = unCryptMailto(s);
+}
