ab96e81907c352553ba3cc0f2d212c9734651952
ngquangvu <nguyenquangvu.work@gmail.com>
2024-05-03 22:35:40 +0700

    update css

--- src/components/organisms/Newsletter/Newsletter.tsx
+++ src/components/organisms/Newsletter/Newsletter.tsx
@@ -28,7 +28,7 @@ export function Newsletter() {
       </h2>
       <p className='mt-2 text-sm text-zinc-600 dark:text-zinc-400'>Get notified when I publish something new, and unsubscribe at any time.</p>
       <div className='mt-6 flex'>
-        <input type='email' name='email' className='h-10 min-w-0 flex-auto appearance-none rounded-lg border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10' placeholder='Email address' aria-label='Email address' required />
+        <input type='email' name='email' className='h-10 min-w-0 flex-auto appearance-none rounded-lg border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10' placeholder='Email address' aria-label='Email address' required />
         <Button type='submit' className='h-10 ml-4 flex items-center  focus:ring-4 focus:ring-teal-500/10 dark:focus:ring-teal-400/10'>
           Join
         </Button>
--- src/components/organisms/SearchSortBar/SearchSortBar.tsx
+++ src/components/organisms/SearchSortBar/SearchSortBar.tsx
@@ -70,7 +70,7 @@ function PostsSearch() {
           setValue(e.target.value);
         }}
         onKeyDown={handleChange}
-        className='w-full h-10 min-w-0 pl-3 pr-8 py-[calc(theme(spacing.2)-1px)]  flex-auto appearance-none rounded-lg border border-zinc-900/10 bg-white shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 sm:text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10'
+        className='w-full h-10 min-w-0 pl-3 pr-8 py-[calc(theme(spacing.2)-1px)]  flex-auto appearance-none rounded-lg border border-zinc-900/10 bg-white shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 text-sm dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10'
         type='search'
         name='search'
         placeholder='Search'
