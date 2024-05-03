855b370386656f1fa2c4285b556de19308aed2cb
ngquangvu <nguyenquangvu.work@gmail.com>
2024-05-03 10:31:24 +0700

    add related posts

--- src/app/blog/[title_id]/page.tsx
+++ src/app/blog/[title_id]/page.tsx
@@ -4,7 +4,8 @@ import { SimpleLayout } from '@/components/layouts/SimpleLayout';
 import { Container } from '@/components/molecules/Container';
 import { PostDetail } from '@/components/molecules/PostDetail';
 import { SkeletonLine } from '@/components/molecules/SkeletonLine';
-import { usePost } from '@/hooks/usePosts';
+import { usePost, usePosts } from '@/hooks/usePosts';
+import { Post, PostsRequestParams } from '@/types/post';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
@@ -13,8 +14,34 @@ export default function BlogPost() {
   const lastSegment = currentPage.split('/').pop();
   const key = lastSegment?.split('-').pop();
 
-  // Get posts
+  // Get post detail
   const { post, postLoading } = usePost(key || '');
+  const currentPost = post;
+
+  // Get related posts
+  const paramCate = (post?.postCategories && post?.postCategories.length > 0 && post?.postCategories[0]?.slug) || '';
+  const paramTag = (post?.postTags && post?.postTags.length > 0 && post?.postTags[0]?.slug) || '';
+  const paramSort = 'relevant';
+  const paramsPostsCate: PostsRequestParams = { limit: 5, page: 1, search: '', cate: paramCate || '', tag: '' || '', sort: paramSort || '', authorId: '' };
+  const paramsPostsTag: PostsRequestParams = { limit: 5, page: 1, search: '', cate: '' || '', tag: paramTag || '', sort: paramSort || '', authorId: '' };
+  const { posts: postsCate, postsLoading: postsLoadingCate } = usePosts(paramsPostsCate);
+  const { posts: postsTag, postsLoading: postsLoadingTag } = usePosts(paramsPostsTag);
+
+  // Merge related posts
+  const relatedPosts: Post[] = [];
+  if (postsCate && postsCate.data?.length > 0) {
+    postsCate.data.forEach((post: Post) => {
+      relatedPosts.push(post);
+    });
+  }
+  if (postsTag && postsTag.data?.length > 0) {
+    postsTag.data.forEach((post: Post) => {
+      relatedPosts.push(post);
+    });
+  }
+
+  // Remove all duplicate posts and current post
+  const uniqueRelatedPosts = relatedPosts.filter((post, index, self) => index === self.findIndex((t) => t.id === post.id) && post.id !== currentPost?.id);
 
   return (
     <>
@@ -27,10 +54,9 @@ export default function BlogPost() {
           </div>
         </Container>
       )}
-      {post && <PostDetail post={post} />}
+      {post && <PostDetail post={post} relatedPosts={uniqueRelatedPosts} />}
       {!postLoading && !post && (
-        <SimpleLayout title='Blog not found'
-          intro='I apologize for any inconvenience, but the requested blog post does not exist. Please verify the URL or try again later.'>
+        <SimpleLayout title='Blog not found' intro='I apologize for any inconvenience, but the requested blog post does not exist. Please verify the URL or try again later.'>
           <div>
             <Link href='/'>Back to Home</Link>
           </div>
--- src/components/molecules/PostCard/PostCard.tsx
+++ src/components/molecules/PostCard/PostCard.tsx
@@ -1,18 +1,31 @@
 import { Post } from '@/types/post';
 import { Card } from '../Card';
 import { formatDate } from '@/utils';
+import Link from 'next/link';
 
 type Props = {
   className?: string;
+  isShowTags?: boolean;
   post: Post;
 };
 
-export const PostCard = ({ className = '', post }: Props) => {
+export const PostCard = ({ className = '', post, isShowTags = false }: Props) => {
   return (
     <Card as="article">
       <Card.Title href={`/blog/${post.url}`}>
         {post.title}
       </Card.Title>
+      {isShowTags && (
+        <div className='relative z-20 flex flex-wrap gap-4 mt-1 mb-2'>
+        {post.postTags &&
+          post?.postTags?.length > 0 &&
+          post?.postTags.map((tag) => (
+            <Link href={`/blog?tag=${tag.slug}`} key={tag.slug} className='text-xs text-zinc-600 dark:text-zinc-400 hover:text-teal-500 dark:hover:text-teal-500 transition-all duration-200'>
+              <span className=''>#{tag.slug}</span>
+            </Link>
+          ))}
+      </div>
+      )}
       <Card.Eyebrow as="time" dateTime={post.postedAtShort} decorate>
         {formatDate(post.postedAtShort || '')}
       </Card.Eyebrow>
