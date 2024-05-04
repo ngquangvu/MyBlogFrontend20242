1dd05b9c5562693750781f123e62b15cd5d2820c
ngquangvu <nguyenquangvu.work@gmail.com>
2024-05-04 21:11:46 +0700

    add test

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { Providers } from '@/app/providers';
 import { Layout } from '@/components/layouts/Layout';
 import '@/styles/tailwind.css';
 import { Suspense } from 'react';
+import Head from 'next/head';
 
 export const metadata: Metadata = {
   title: {
@@ -17,6 +18,9 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang='en' className='h-full antialiased' suppressHydrationWarning>
       <body suppressHydrationWarning={true} className='flex h-full bg-zinc-50 dark:bg-black'>
+        <Head>
+          <meta http-equiv='Content-Security-Policy' content='upgrade-insecure-requests' />
+        </Head>
         <Providers>
           <div className='flex w-full'>
             <Suspense>
